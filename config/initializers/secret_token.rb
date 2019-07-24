01f3b767a21c15e10f48babc911abe3b38bb45b2
gokulparthiban123 <gokulparthiban123@gmail.com>
2019-07-24 15:45:30 +0530

    Init folder structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '2eef12adcf54f7e7b3442d8ba39f9a5a32b04d5fea779b763a40a765d8fe7d518bbb6d38fc05ea392b1ff8362677fbe1b119a7333488445e2e49e0720c973d50'
